218eff063c0de8c6b5953316f378d2c681e139b5
Joshua Eom <joshuaeom132@gmail.com>
2024-05-06 20:15:54 -0400

    5/3/2024 - Base HTML page + structured overall file layout + A little bootstrap

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// // Runs once the HTML page has loaded
+// document.addEventListener("DOMContentLoaded", function() {
+//     fetchIndexContent();
+// })
+
+// function fetchIndexContent(){
+//     fetch("index-content.json")
+//         .then(response => response.json())
+//         .then(data => {
+
+//         })
+// }
